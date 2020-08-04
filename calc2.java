bf22b1b8b4f30739338fc0d11f7481570080dd03
Spandan Ghosh <spandu500@gmail.com>
2020-08-04 11:48:08 +0530

    Make Functions Non-Static

--- calc2.java
+++ calc2.java
@@ -1,31 +1,31 @@
 import java.util.Scanner;               //Used Static Functions with parameters
 
 public class calc2 {
-    static double add(double a, double b) {
+    double add(double a, double b) {
         System.out.println("Adding Two Numbers...");
         double res = a + b;
         return res;
     }
 
-    static double sub(double a, double b) {
+    double sub(double a, double b) {
         System.out.println("Subtracting Two Numbers...");
         double res = a - b;
         return res;
     }
 
-    static double mul(double a, double b) {
+    double mul(double a, double b) {
         System.out.println("Multiplying Two Numbers...");
         double res = a * b;
         return res;
     }
 
-    static double div(double a, double b) {
+    double div(double a, double b) {
         System.out.println("Dividing Two Numbers...");
         double res = a / b;
         return res;
     }
 
-    static double rem(double a, double b) {
+    double rem(double a, double b) {
         System.out.println("Calculating Remainder...");
         double res = a % b;
         return res;
@@ -34,7 +34,7 @@ public class calc2 {
     public static void main(String[] args) {
         double a, b, res;
         Scanner s = new Scanner(System.in);
-
+        calc2 cx = new calc2();
         System.out.print("Enter 2 numbers: ");
         a = s.nextDouble();
         b = s.nextDouble();
@@ -43,19 +43,19 @@ public class calc2 {
         s.close();
         switch (op) {
             case '+':
-                res=add(a, b);
+                res=cx.add(a, b);
                 break;
             case '-':
-                res=sub(a, b);
+                res=cx.sub(a, b);
                 break;
             case '/':
-                res=div(a, b);
+                res=cx.div(a, b);
                 break;
             case '*':
-                res=mul(a, b);
+                res=cx.mul(a, b);
                 break;
             case '%':
-                res=rem(a, b);
+                res=cx.rem(a, b);
                 break;
             default:
             System.out.println("Error enter correct operator");
