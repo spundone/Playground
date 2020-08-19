66fece8dc2a0c99c71785b222c72441c0194d0fc
Spandan Ghosh <spandu500@gmail.com>
2020-08-19 20:51:40 +0530

    add derped stuff

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test 
+{
+    public static void main(String[] args) //Fuction    (This is a comment)
+    {
+        Double a=16.89;
+        Boolean b = false;
+        int c=10;
+        float d=1.6f;
+        
+        System.out.println("Double a ="+a);
+        System.out.println("Boolean b is declared "+b);
+        System.out.println("Int c is "+c);
+        System.out.println("Float d="+d);
+    }
+}
