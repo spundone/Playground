8e58afa2d34040a9cdc64c0b05b892b0a4ad4ce2
Spandan Ghosh <spandu500@gmail.com>
2020-08-28 13:10:04 +0530

    Fix String Line Input, add higher salary support

--- Salary.java
+++ Salary.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Salary {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the empno");
+        System.out.print("Enter the name of the employee : ");
+        String empname = sc.nextLine();
+        System.out.print("Enter the employee no : ");
         int empno = sc.nextInt();
-        System.out.println("Enter the name of the employee");
-        String empname = sc.next();
-        System.out.println("Enter the basic salary");
-        int sal = sc.nextInt();
+        System.out.print("Enter the basic salary : ");
+        Double sal = sc.nextDouble();
         double da = 0.70 * sal, hra = 0.30 * sal, pf = 0.10 * sal;
         int cca = 240, pt = 100;
         double gsal = sal + da + hra + cca + pt;
