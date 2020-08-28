e2c80b96d5dab91332330173718bbbe587383380
Spandan Ghosh <spandu500@gmail.com>
2020-08-28 15:36:24 +0530

    Made Scanner global

--- Bank.java
+++ Bank.java
@@ -4,32 +4,30 @@ public class Bank {
 
     public static double balance, interestRate;
     public static String name, address, appNumber, phone;
-    public static String seperator = "****************************************************************";
+    public static final String SEPERATOR = "*******************************************************************************";
+    public static final Scanner sc = new Scanner(System.in);
+
 
     static void deposit() {
-        final Scanner sc = new Scanner(System.in);
-        System.out.println(Bank.seperator);
+        System.out.println(Bank.SEPERATOR);
         System.out.println("Account Balance is " + Bank.balance);
         System.out.print("Enter Amount to Deposit :");
-        final double depositAmount = sc.nextDouble();
+        double depositAmount = sc.nextDouble();
         Bank.balance += depositAmount;
         System.out.println("Amount Deposited...");
         System.out.println("Balance Amount after Deposit : " + Bank.balance);
-        System.out.println(Bank.seperator);
-        //sc.close();
+        System.out.println(Bank.SEPERATOR);
     }
 
     static void withdraw() {
-        System.out.println(Bank.seperator);
-        final Scanner sc = new Scanner(System.in);
+        System.out.println(Bank.SEPERATOR);
         System.out.println("Account Balance is " + Bank.balance);
         System.out.print("Enter Amount to Withdraw :");
-        final double withdrawAmount = sc.nextDouble();
+        double withdrawAmount = sc.nextDouble();
         Bank.balance -= withdrawAmount;
         System.out.println("Amount Withdrawed...");
         System.out.println("Balance Amount after Withdrawal : " + Bank.balance);
-        System.out.println(Bank.seperator);
-        //sc.close();
+        System.out.println(Bank.SEPERATOR);
     }
 
     static void computeInterest() {
@@ -41,8 +39,7 @@ public class Bank {
     }
 
     static void createAccount() {
-        System.out.println(Bank.seperator);
-        final Scanner sc = new Scanner(System.in);
+        System.out.println(Bank.SEPERATOR);
         System.out.print("Enter your full name : ");
         Bank.name = sc.nextLine();
         System.out.println("Enter your Address Below...");
@@ -51,7 +48,7 @@ public class Bank {
         do {
             System.out.print("Enter your Contact Number : ");
             Bank.phone = sc.next();
-            final String regex = "\\d{10}";
+            String regex = "\\d{10}";
             result = Bank.phone.matches(regex);
             if (result) {
                 System.out.println("Given phone number is valid");
@@ -64,21 +61,19 @@ public class Bank {
         System.out.println("Deposit Initial Account...");
         Bank.deposit();
         System.out.println("Account Created Sucesssfully");
-        System.out.println(Bank.seperator);
-        //sc.close();
+        System.out.println(Bank.SEPERATOR);
     }
 
     public static void main(final String[] args) {
         int flag = 0;
-        final Scanner sc = new Scanner(System.in);
         do {
-            System.out.println(Bank.seperator);
+            System.out.println(Bank.SEPERATOR);
             System.out.println("Welcome to the Java Bank");
             System.out.println("1: Open Account");
             System.out.println("2: Deposit Money");
             System.out.println("3: Withdraw Money");
             System.out.println("4: Display Account Info");
-            System.out.println(Bank.seperator);
+            System.out.println(Bank.SEPERATOR);
             System.out.println("Enter Your Choice");
             final int choice = sc.nextInt();
             switch (choice) {
@@ -92,7 +87,7 @@ public class Bank {
                     Bank.withdraw();
                     break;
                 case 4:
-                    System.out.println(Bank.seperator);
+                    System.out.println(Bank.SEPERATOR);
                     System.out.println("Hello " + Bank.name);
                     System.out.println("Your Application No is " + Bank.appNumber);
                     System.out.println("Your Bank Balance is " + Bank.balance);
@@ -100,16 +95,15 @@ public class Bank {
                     System.out.println("You Currently live at " + Bank.address);
                     Bank.computeInterest();
                     System.out.println("Thank You For using Java Bank");
-                    System.out.println(Bank.seperator);
+                    System.out.println(Bank.SEPERATOR);
                     break;
                 default:
                     System.out.println("Error enter correct option");
             }
-            System.out.println("Do You Want to Quit? (Press Y) Else Press any key to Continue ");
-            final char opt = sc.next().charAt(0);
+            System.out.println("Do You Want to Quit? (Press Y) Else Press any Char and Enter to Continue ");
+            char opt = sc.next().charAt(0);
             if (opt == 'Y' || opt == 'y')
                 flag++;
         } while (flag == 0);
-        //sc.close();
     }
 }
