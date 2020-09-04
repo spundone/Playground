1513bda5384a76b8b854a120d6014a1851a910d3
Spandan Ghosh <spandu500@gmail.com>
2020-09-05 00:53:14 +0530

    Add Basic Assignment InvertStar.java

---
+++ Java/InvertStar.java
@@ -0,0 +1,18 @@
+package Java;       //Made by Spandan Ghosh
+                    //Available at github.com/spandu500/playground/Java/InvertStar.java
+import java.util.Scanner;
+
+public class InvertStar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Length of the pattern (n): ");
+        int n = sc.nextInt();
+        for (int i = n; i > 0; i--) {           //controls no of j prints indirectly (-)inverted order
+            for (int j = 0; j < i; j++) {       //prints current i no of stars
+                System.out.print("* ");
+            }
+            System.out.println();               //prints newline
+            sc.close();
+        }
+    }
+}
