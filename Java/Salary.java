2e68bbc9b3d80691569105c8b65d20ccc6eef789
Spandan Ghosh <spandu500@gmail.com>
2020-09-05 16:52:26 +0530

    Add pf to gsal

--- Java/Salary.java
+++ Java/Salary.java
@@ -13,7 +13,7 @@ public class Salary {
         double bsal = sc.nextDouble();
         double da = 0.70 * bsal, hra = 0.30 * bsal, pf = 0.10 * bsal;
         int cca = 240, pt = 100;
-        double gsal = bsal + da + hra + cca + pt;
+        double gsal = bsal + da + hra + cca + pf + pt;
         double nsal = gsal - (cca + pf + pt);
         System.out.println("❶ Employee Name   : " + empname);
         System.out.println("❷ Employee Number : " + empno);
