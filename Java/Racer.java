c7927bdf11fa6dfff821f4e4775bac61787f8101
Spandan Ghosh <spandu500@gmail.com>
2020-09-16 01:39:47 +0530

    Add URL

--- Java/Racer.java
+++ Java/Racer.java
@@ -1,5 +1,5 @@
-package Java;
-import java.util.*;
+package Java;                            //Made by Spandan Ghosh
+import java.util.*;                     //Available at https://github.com/spandu500/Playground/tree/master/Java/Racer.java
 
 public class Racer {
     public static void main(String[] args)
